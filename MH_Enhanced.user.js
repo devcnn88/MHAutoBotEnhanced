78cc6a755560de73484e4a61afb8209ed75b34e1
nnchoy <nnchoy@genericpower.com>
2016-03-24 10:38:55 +0800

    Enable SCCustom

--- MH_Enhanced.user.js
+++ MH_Enhanced.user.js
@@ -2495,7 +2495,7 @@ function embedTimer(targetPage) {
 			preferenceHTMLStr += '<option value="Halloween 2015">Halloween 2015</option>';
 			preferenceHTMLStr += '<option value="Sunken City">Sunken City</option>';
 			preferenceHTMLStr += '<option value="Sunken City Aggro">Sunken City Aggro</option>';
-			//preferenceHTMLStr += '<option value="Sunken City Custom">Sunken City Custom</option>';
+			preferenceHTMLStr += '<option value="Sunken City Custom">Sunken City Custom</option>';
             preferenceHTMLStr += '</select>';
             preferenceHTMLStr += '</td>';
             preferenceHTMLStr += '</tr>';
