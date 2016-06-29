9546f634ff2470faea948b4878d0540440164dfa
nnchoy <nnchoy@genericpower.com>
2016-06-29 13:42:04 +0800

    Labeled ver 1.31.0

--- MH_Enhanced.user.js
+++ MH_Enhanced.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MouseHunt AutoBot Enhanced Edition
 // @author      Ooi Keng Siang, CnN
-// @version    	1.30.1
+// @version    	1.31.0
 // @namespace   http://ooiks.com/blog/mousehunt-autobot, https://devcnn.wordpress.com/
 // @description Ooiks: An advance user script to automate sounding the hunter horn in MouseHunt application in Facebook with MouseHunt version 3.0 (Longtail) supported and many other features. CnN: An enhanced version to sound horn based on selected algorithm of event or location.
 // @require		https://code.jquery.com/jquery-2.2.2.min.js
@@ -261,7 +261,7 @@ var objLength = {
 // WARNING - Do not modify the code below unless you know how to read and write the script.
 
 // All global variable declaration and default value
-var scriptVersion = "1.30.1 Enhanced Edition";
+var scriptVersion = "1.31.0 Enhanced Edition";
 var fbPlatform = false;
 var hiFivePlatform = false;
 var mhPlatform = false;
