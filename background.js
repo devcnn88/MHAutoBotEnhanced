df08784a384fa0fe9cb375a6ca244cd8f78a9c8b
nnchoy <nnchoy@genericpower.com>
2016-06-30 15:25:16 +0800

    Added new file

---
+++ background.js
@@ -0,0 +1,22 @@
+var mainPort;
+var trapsPort;
+chrome.runtime.onConnect.addListener(function(port) {
+	if(port.name == 'main'){
+		mainPort = port;
+		mainPort.onMessage.addListener(function(msg) {
+			if (msg.request == "get"){
+				if(trapsPort !== null || trapsPort !== undefined)
+					trapsPort.postMessage(msg);
+			}
+		});
+	}
+	else if(port.name == 'traps'){
+		trapsPort = port;
+		trapsPort.onMessage.addListener(function(msg) {
+			if (Array.isArray(msg.result)){
+				if(mainPort !== null || mainPort !== undefined)
+					mainPort.postMessage(msg);
+			}
+		});
+	}
+});
