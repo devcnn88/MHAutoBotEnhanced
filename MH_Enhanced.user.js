73b5bd62eb8c9317208c98d7da3aeece1ab9f894
nnchoy <nnchoy@genericpower.com>
2016-07-07 10:26:02 +0800

    Added ajax post

--- MH_Enhanced.user.js
+++ MH_Enhanced.user.js
@@ -954,6 +954,25 @@ function checkCaughtMouse(obj, arrUpdatedUncaught){
 			arrUncaughtMouse = [];
 			if(getMapPort === null || getMapPort === undefined){
 				// direct call jquery
+				var objData = {
+					sn : 'Hitgrab',
+					hg_is_ajax : 1,
+					action : 'info',
+					uh : getPageVariable('user.unique_hash')
+				};
+				ajaxPost(window.location.origin + '/managers/ajax/users/relichunter.php', objData, function (data){
+					console.log(data.treasure_map);
+					if(data.treasure_map.groups !== null && data.treasure_map.groups !== undefined){
+						var arrUncaught = new Array(data.treasure_map.groups[0].mice.length);
+						for(var i=0;i<data.treasure_map.groups[0].mice.length;i++){
+							arrUncaught[i] = data.treasure_map.groups[0].mice[i].name;
+						}
+						if(arrUncaught.length > 0)
+							checkCaughtMouse(obj, arrUncaught);
+					}
+				}, function (error){
+					console.error('ajax:',error);
+				});
 			}
 			else{
 				getMapPort.postMessage({
@@ -4754,6 +4773,26 @@ function CalculateNextTrapCheckInMinute() {
 // ################################################################################################
 //   General Function - Start
 // ################################################################################################
+
+function ajaxPost(postURL, objData, callback, throwerror){
+	try {
+		jQuery.ajax({
+			type: 'POST',
+			url: postURL,
+			data: objData,
+			contentType: 'application/x-www-form-urlencoded',
+			dataType: 'json',
+			xhrFields: {
+				withCredentials: false
+			},
+			success: callback,
+			error: throwerror, 
+		});
+	}
+	catch (e) {
+		throwerror(e);
+	}
+}
 
 function isNullOrUndefined(obj){
 	return (obj === null || obj === undefined);
