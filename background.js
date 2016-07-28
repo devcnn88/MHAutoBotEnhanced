80c5d5354722861f05620f4187ba8517c9c78fde
nnchoy <nnchoy@genericpower.com>
2016-07-28 11:06:33 +0800

    1. Insert trap collection into main script 2. Remove unnecessary extension port 3. Update MapHunting to Season 4

--- background.js
+++ background.js
@@ -1,45 +1,21 @@
-var mainPort;
-var trapsPort;
 var mapPort;
 
 chrome.runtime.onConnect.addListener(function(port) {
-	if(port.name == 'main'){
-		mainPort = port;
-		mainPort.onMessage.addListener(function(msg) {
-			if (msg.request == "get"){
-				if(trapsPort !== null && trapsPort !== undefined){
-					trapsPort.postMessage(msg);
-				}
-			}
-		});
-	}
-	else if(port.name == 'traps'){
-		trapsPort = port;
-		trapsPort.onMessage.addListener(function(msg) {
-			if (Array.isArray(msg.result)){
-				if(mainPort !== null && mainPort !== undefined){
-					mainPort.postMessage(msg);
-				}
-			}
-		});
-	}
-	else if(port.name == 'map'){
+	if(port.name == 'map'){
 		mapPort = port;
 		mapPort.onMessage.addListener(function(msg) {
 			console.log(msg);
 			if (msg.request == "getUncaught"){
-				var objData = {
-					sn : 'Hitgrab',
-					hg_is_ajax : 1,
-					action : 'info',
-					uh : msg.unique_hash
-				};
-				ajaxPost(msg.url, objData, function (data){
+				ajaxPost(msg.url, msg.data, function (data){
 					console.log(data.treasure_map);
 					if(data.treasure_map.groups !== null && data.treasure_map.groups !== undefined){
-						var arrUncaught = new Array(data.treasure_map.groups[0].mice.length);
-						for(var i=0;i<data.treasure_map.groups[0].mice.length;i++){
-							arrUncaught[i] = data.treasure_map.groups[0].mice[i].name;
+						var arrUncaught = [];
+						for(var i=0;i<data.treasure_map.groups.length;i++){
+							if(data.treasure_map.groups[i].is_uncaught === true){
+								for(var j=0;j<data.treasure_map.groups[i].mice.length;j++){
+									arrUncaught.push(data.treasure_map.groups[i].mice[j].name);
+								}
+							}
 						}
 						mapPort.postMessage({
 							obj : msg.objMapHunting,
